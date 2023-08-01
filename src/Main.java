8607624df352bfb14833a9165cd0c750757d2b38
tarragoosorio <141117869+tarragoosorio@users.noreply.github.com>
2023-08-01 14:44:07 +0300

    e mail added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Привееееетикиииии");
+        String s = "sekret1703@yandex.ru";
+        System.out.println("Почта для связи " + s);
     }
 }
